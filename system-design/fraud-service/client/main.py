3cdadd52bf5180f3d8b540604a4acc652f14ca6b
Gustavo Gardusi <gustavo.gardusi@gmail.com>
2023-11-03 16:36:33 -0300

    Updated client code

--- system-design/fraud-service/client/main.py
+++ system-design/fraud-service/client/main.py
@@ -1,13 +1,36 @@
-if __name__ == "__main__":
-    client = FraudServiceClient("localhost", 50051)
+from client import FraudServiceClient
+from google.protobuf.timestamp_pb2 import Timestamp
+from proto.generated import transaction_pb2
+import datetime
 
-    request = transaction_pb2.IsTransactionValidRequest(
-        transactionId="12345",
-        merchantId="merchant123",
-        userId="user123",
-        cardNumber="1234-5678-9012-3456",
-        # ... fill in other fields ...
+
+def get_timestamp_from_str(timestamp: str) -> Timestamp:
+    datetime_obj = datetime.datetime.fromisoformat(timestamp)
+    timestamp_message = Timestamp()
+    timestamp_message.FromDatetime(datetime_obj)
+    return timestamp_message
+
+
+def generate_request() -> transaction_pb2.IsTransactionValidRequest:
+    return transaction_pb2.IsTransactionValidRequest(
+        transactionId="21320398",
+        merchantId="29744",
+        userId="97051",
+        cardNumber="434505******9116",
+        transactionDate=get_timestamp_from_str('2019-12-01T23:16:32.812632'),
+        transactionAmount=374.56,
+        deviceId='285475',
+        hasCbk=False,
     )
 
+
+def main():
+    client = FraudServiceClient("localhost", 50051)
+    print('Created client')
+    request = generate_request()
     response = client.is_transaction_valid(request)
     print(f"Is Transaction Valid: {response.isValid}")
+
+
+if __name__ == "__main__":
+    main()
