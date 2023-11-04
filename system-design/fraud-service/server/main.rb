74eb4d04f94a528b48b70aaaea2c6ff81d4a55cb
Gustavo Gardusi <gustavo.gardusi@gmail.com>
2023-11-04 16:06:25 -0300

    Updated server

--- system-design/fraud-service/server/main.rb
+++ system-design/fraud-service/server/main.rb
@@ -5,7 +5,7 @@ $stdout.flush
 server = GRPC::RpcServer.new
 puts 'Running the server...'
 $stdout.flush
-server.add_http2_port('localhost:50051', :this_port_is_insecure)
+server.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
 server.handle(FraudServiceImpl.new)
 server.run_till_terminated
 puts 'Server is running.'
