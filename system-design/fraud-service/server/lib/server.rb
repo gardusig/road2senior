319edb0145f279a04003f6e7f61466c76f1b1d89
Gustavo Gardusi <gustavo.gardusi@gmail.com>
2023-11-05 19:55:26 -0300

    Updated server validation to reject requests with hasCbk

--- system-design/fraud-service/server/lib/server.rb
+++ system-design/fraud-service/server/lib/server.rb
@@ -3,11 +3,19 @@ require_relative '../proto/generated/transaction_services_pb'
 
 class FraudServiceImpl < FraudService::FraudService::Service
   def is_transaction_valid(request, _call)
-    puts "Received a request for transaction ID: #{request.transactionId}"
+    puts "Received a request. transactionId: #{request.transactionId}, cbk: #{request.hasCbk}"
     response = FraudService::IsTransactionValidResponse.new
     response.transactionId = request.transactionId
-    response.isValid = true
-    puts "Sending a response with transaction ID: #{response.transactionId}"
-    response
+    response.isValid = is_request_valid(request)
+    puts "Sending response. transactionId: #{response.transactionId}, isValid: #{response.isValid}"
+    return response
+  end
+
+  def is_request_valid(request)
+    if request.hasCbk == true
+      return false
+    end
+    # Additional steps or logic can go here
+    return true
   end
 end
