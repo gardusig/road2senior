cd1460d29dd5eb399fd016c95e5a4723b00be321
Gustavo Gardusi <gustavo.gardusi@gmail.com>
2023-12-08 17:13:42 -0300

    Added project structure for advent of code

---
+++ problem-solving/advent-of-code/src/main/java/aoc/year2023/day1/Day1.java
@@ -0,0 +1,7 @@
+package aoc.year2023.day1;
+
+public class Day1 {
+    public static void main(final String args[]) {
+
+    }
+}
---
+++ problem-solving/advent-of-code/src/test/java/aoc/year2023/day1/Day1Test.java
@@ -0,0 +1,11 @@
+package aoc.year2023.day1;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Day1Test {
+    @Test
+    public void testExampleInput() {
+        assertEquals("kappa", "kappa");
+    }
+}
